04539703bdd1b5069c178ee2d75c3683132b050c
88guri <quffl2002@naver.com>
2026-01-08 21:39:57 +0900

    feat: #15 Member 도메인 추가

---
+++ src/main/java/org/sopt/poti/domain/artist/entity/Member.java
@@ -0,0 +1,36 @@
+package org.sopt.poti.domain.artist.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "members")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 255)
+    private String name;
+
+    @Column(name = "member_image_url", length = 255)
+    private String memberImageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artist_id", nullable = false)
+    private Artist artist;
+
+    public static Member create(Long id, String name, String memberImageUrl, Artist artist) {
+        Member member = new Member();
+        member.id = id;
+        member.name = name;
+        member.memberImageUrl = memberImageUrl;
+        member.artist = artist;
+        return member;
+    }
+}
