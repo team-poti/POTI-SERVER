b5416006ebe6a60fca8b8ec0953389123ff755b5
88guri <quffl2002@naver.com>
2026-01-08 21:40:17 +0900

    feat: #15 OrderItem 도메인 추가

---
+++ src/main/java/org/sopt/poti/domain/order/entity/OrderItem.java
@@ -0,0 +1,45 @@
+package org.sopt.poti.domain.order.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.poti.domain.groupbuy.entity.GroupBuyOption;
+
+@Getter
+@Entity
+@Table(name = "order_items")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int count;
+
+    @Column(name = "unit_price", nullable = false)
+    private int unitPrice;
+
+    @Column(name = "item_amount", nullable = false)
+    private int itemAmount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_buy_option_id", nullable = false)
+    private GroupBuyOption groupBuyOption;
+
+    public static OrderItem create(Long id, int count, int unitPrice, GroupBuyOption option) {
+        OrderItem oi = new OrderItem();
+        oi.id = id;
+        oi.count = count;
+        oi.unitPrice = unitPrice;
+        oi.itemAmount = count * unitPrice;
+        oi.groupBuyOption = option;
+        return oi;
+    }
+}
