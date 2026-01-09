ad6d4869b8d09992baddd6403e25d71e72a75cdd
88guri <quffl2002@naver.com>
2026-01-09 16:27:04 +0900

    feat: #15 분철글 자체 상태값 추가 (입금완료, 배송시작, 배송완료)

--- src/main/java/org/sopt/poti/domain/groupbuy/entity/GroupBuyPostStatus.java
+++ src/main/java/org/sopt/poti/domain/groupbuy/entity/GroupBuyPostStatus.java
@@ -1,7 +1,11 @@
 package org.sopt.poti.domain.groupbuy.entity;
 
+//분철글 자체의 상태값
 public enum GroupBuyPostStatus {
     RECRUITING, //모집중
     CLOSED, //모집 완료
+    PAYMENT_DONE,//입금완료
+    SHIPPING,//배송시작
+    DELIVERED,//배송완료
     COMPLETED //거래 완료
 }
