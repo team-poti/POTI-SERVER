cbad75f20f32e5a3ca476c8e92b401c211ac3979
88guri <quffl2002@naver.com>
2026-01-09 16:55:39 +0900

    feat: #15 Delivery 상태값을 OrderStatus에 추가

--- src/main/java/org/sopt/poti/domain/order/entity/OrderStatus.java
+++ src/main/java/org/sopt/poti/domain/order/entity/OrderStatus.java
@@ -3,5 +3,10 @@ package org.sopt.poti.domain.order.entity;
 public enum OrderStatus {
     WAIT_PAY, // 입금 대기
     WAIT_PAY_CHECK, //입금 확인 대기
-    PAID //입금 완료
+    PAID, //입금 완료
+
+    //배송 관련 상태값
+    READY, //배송 준비중
+    SHIPPED, //배송중
+    DELIVERED //배송 완료
 }
