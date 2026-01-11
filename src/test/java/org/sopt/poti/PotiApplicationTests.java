32f34d78da5c575e1129f0f242c914d720010d24
pbem22 <dlacofbs1229@gmail.com>
2026-01-11 17:40:27 +0900

    fix: activeprofiles test, 스프링 프로필 환경변수 주입 방식으로 변경

--- src/test/java/org/sopt/poti/PotiApplicationTests.java
+++ src/test/java/org/sopt/poti/PotiApplicationTests.java
@@ -2,12 +2,14 @@ package org.sopt.poti;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class PotiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
 }
