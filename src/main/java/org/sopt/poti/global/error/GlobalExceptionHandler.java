bda37ab64558c5c55ebdd8bd0dc8f67ebf0a27a4
88guri <quffl2002@naver.com>
2026-01-12 01:27:40 +0900

    chore: #20 줄바꿈

--- src/main/java/org/sopt/poti/global/error/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/poti/global/error/GlobalExceptionHandler.java
@@ -3,19 +3,20 @@ package org.sopt.poti.global.error;
 import org.sopt.poti.global.common.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
 
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-        ApiResponse.fail(ErrorStatus.INTERNAL_SERVER_ERROR)
-    );
-  }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                ApiResponse.fail(ErrorStatus.INTERNAL_SERVER_ERROR)
+        );
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<Void>> handleValidation(
@@ -29,8 +30,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ApiResponse<Void>> handleBusinessException(BusinessException e) {
 
-    return ResponseEntity.status(e.getErrorStatus().getHttpStatus()).body(
-        ApiResponse.fail(e.getErrorStatus())
-    );
-  }
+        return ResponseEntity.status(e.getErrorStatus().getHttpStatus()).body(
+                ApiResponse.fail(e.getErrorStatus())
+        );
+    }
 }
