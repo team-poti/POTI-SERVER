67be6f64d91eca3bce475909b53179d81a270d67
88guri <quffl2002@naver.com>
2026-01-12 01:22:43 +0900

    feat: #20 온보딩 관련 Service 추가

---
+++ src/main/java/org/sopt/poti/domain/user/service/UserService.java
@@ -0,0 +1,43 @@
+package org.sopt.poti.domain.user.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.sopt.poti.domain.artist.entity.Artist;
+import org.sopt.poti.domain.artist.repository.ArtistRepository;
+import org.sopt.poti.domain.user.dto.request.UserOnboardingRequest;
+import org.sopt.poti.domain.user.dto.response.UserOnboardingResponse;
+import org.sopt.poti.domain.user.entity.User;
+import org.sopt.poti.domain.user.repository.UserRepository;
+import org.sopt.poti.global.error.BusinessException;
+import org.sopt.poti.global.error.ErrorStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final ArtistRepository artistRepository;
+
+    @Transactional
+    public UserOnboardingResponse saveOnboarding(Long userId, UserOnboardingRequest req) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorStatus.USER_NOT_FOUND));
+
+        user.updateNickname(req.nickname());
+
+        Artist favorite = null;
+        if (req.favoriteArtistId() != null) {
+            favorite = artistRepository.findById(req.favoriteArtistId())
+                    .orElseThrow(() -> new BusinessException(ErrorStatus.ARTIST_NOT_FOUND));
+        }
+        user.updateFavoriteArtist(favorite);
+
+        return new UserOnboardingResponse(
+                user.getNickname(),
+                user.getFavoriteArtist() == null ? null : user.getFavoriteArtist().getId()
+        );
+    }
+
+}
