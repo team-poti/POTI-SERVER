f4789d92d9389a5710f67dbab62c1d41551fbe7b
88guri <quffl2002@naver.com>
2026-01-13 02:40:19 +0900

    feat: #20 @notblank @Size로 예외 메시지 지정

--- src/main/java/org/sopt/poti/domain/user/dto/request/NicknameDuplicateRequest.java
+++ src/main/java/org/sopt/poti/domain/user/dto/request/NicknameDuplicateRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record NicknameDuplicateRequest(
-        @NotBlank
-        @Size(min = 2, max = 10)
+        @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+        @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
         String nickname
 ) {}
