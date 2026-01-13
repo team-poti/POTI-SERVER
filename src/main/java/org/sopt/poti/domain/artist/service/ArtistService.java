fa0ee1ff2cd4121e7cc29a202eb6041a9aac2486
88guri <quffl2002@naver.com>
2026-01-14 01:07:32 +0900

    feat: #37 아티스트 리스트 관련 getArtist 메서드 추가

--- src/main/java/org/sopt/poti/domain/artist/service/ArtistService.java
+++ src/main/java/org/sopt/poti/domain/artist/service/ArtistService.java
@@ -1,12 +1,15 @@
 package org.sopt.poti.domain.artist.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.poti.domain.artist.dto.response.ArtistListResponse;
 import org.sopt.poti.domain.artist.entity.Artist;
 import org.sopt.poti.domain.artist.repository.ArtistRepository;
 import org.sopt.poti.global.error.BusinessException;
 import org.sopt.poti.global.error.ErrorStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +21,19 @@ public class ArtistService {
     public Artist getById(Long artistId) {
         return artistRepository.findById(artistId)
                 .orElseThrow(() -> new BusinessException(ErrorStatus.ARTIST_NOT_FOUND));
+    }
+
+    public ArtistListResponse getArtists() {
+        List<Artist> artists = artistRepository.findAll();
+
+        return new ArtistListResponse(
+                artists.stream()
+                        .map(a -> new ArtistListResponse.ArtistItemResponse(
+                                a.getId(),
+                                a.getName(),
+                                a.getLogoImageUrl()
+                        ))
+                        .toList()
+        );
     }
 }
