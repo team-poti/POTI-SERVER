3d427d4f359d4710280152d1eb2d0395fbaa5940
88guri <quffl2002@naver.com>
2026-01-13 21:55:50 +0900

    chore: #35 스웨거 문서 추가

--- src/main/java/org/sopt/poti/domain/user/controller/MyPageController.java
+++ src/main/java/org/sopt/poti/domain/user/controller/MyPageController.java
@@ -1,5 +1,7 @@
 package org.sopt.poti.domain.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.sopt.poti.domain.user.dto.response.MyPageResponse;
 import org.sopt.poti.domain.user.service.MyPageService;
@@ -15,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
+@Tag(name = "MyPage", description = "마이페이지 관련 API")
 public class MyPageController {
 
     private final MyPageService myPageService;
 
     @GetMapping("/mypage")
+    @Operation(summary = "마이페이지 조회", description = "로그인한 유저 본인의 마이페이지를 조회합니다.")
     public ResponseEntity<ApiResponse<MyPageResponse>> getMyPage(
             @AuthenticationPrincipal UserPrincipal userPrincipal
     ) {
