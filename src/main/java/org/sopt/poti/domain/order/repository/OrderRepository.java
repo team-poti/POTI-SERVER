4b9ded291148af75ac5aa2c5560cc1ab725723d9
88guri <quffl2002@naver.com>
2026-01-14 21:43:49 +0900

    feat: #43 내 참여 내역 리스트 Repository 추가

--- src/main/java/org/sopt/poti/domain/order/repository/OrderRepository.java
+++ src/main/java/org/sopt/poti/domain/order/repository/OrderRepository.java
@@ -3,6 +3,7 @@ package org.sopt.poti.domain.order.repository;
 import java.util.List;
 import org.sopt.poti.domain.order.entity.Order;
 import org.sopt.poti.domain.order.entity.OrderStatus;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
@@ -10,4 +11,10 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     int countByUser_Id(Long userId);
 
     int countByUser_IdAndStatusIn(Long userId, List<OrderStatus> statuses);
+
+    @EntityGraph(attributePaths = {
+            "groupBuyPost",
+            "groupBuyPost.artist"
+    })
+    List<Order> findByUser_IdOrderByCreatedAtDesc(Long userId);
 }
