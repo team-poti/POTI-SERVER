13e8889dc8f3968434476be556e1fb6be8439149
88guri <quffl2002@naver.com>
2026-01-15 00:23:11 +0900

    feat: #43 스웨거 설명 추가

--- src/main/java/org/sopt/poti/domain/participation/controller/ParticipationController.java
+++ src/main/java/org/sopt/poti/domain/participation/controller/ParticipationController.java
@@ -1,5 +1,7 @@
 package org.sopt.poti.domain.participation.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.sopt.poti.domain.participation.dto.response.ParticipationSummaryResponse;
 import org.sopt.poti.domain.participation.entity.ParticipationStatus;
@@ -16,12 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Participation", description = "공구 참여 관련 API")
 @RequestMapping("/api/v1/participations")
 public class ParticipationController {
 
     private final ParticipationService participationService;
 
     @GetMapping
+    @Operation(summary = "내 공구 참여 내역 리스트", description = "로그인한 유저가 구매자로 참여한 공구 참여 내역 리스트를 반환합니다.")
     public ResponseEntity<ApiResponse<ParticipationSummaryResponse>> getMyParticipations(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
             @RequestParam ParticipationStatus status
