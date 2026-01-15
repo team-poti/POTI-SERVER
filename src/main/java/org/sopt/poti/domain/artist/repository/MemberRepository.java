663cc30738c1b43331642b54ec6db1b85017a661
pbem22 <dlacofbs1229@gmail.com>
2026-01-15 17:08:27 +0900

    feat: #58 아티스트별 멤버 목록 조회 API 구현

--- src/main/java/org/sopt/poti/domain/artist/repository/MemberRepository.java
+++ src/main/java/org/sopt/poti/domain/artist/repository/MemberRepository.java
@@ -1,9 +1,13 @@
 package org.sopt.poti.domain.artist.repository;
 
+import java.util.List;
+import org.sopt.poti.domain.artist.entity.Artist;
 import org.sopt.poti.domain.artist.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+  List<Member> findByArtist(Artist artist);
 }
--- src/main/java/org/sopt/poti/domain/artist/service/ArtistService.java
+++ src/main/java/org/sopt/poti/domain/artist/service/ArtistService.java
@@ -1,39 +1,54 @@
 package org.sopt.poti.domain.artist.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.sopt.poti.domain.artist.dto.response.ArtistListResponse;
+import org.sopt.poti.domain.artist.dto.response.MemberListResponse;
+import org.sopt.poti.domain.artist.dto.response.MemberListResponse.MemberResponse;
 import org.sopt.poti.domain.artist.entity.Artist;
 import org.sopt.poti.domain.artist.repository.ArtistRepository;
+import org.sopt.poti.domain.artist.repository.MemberRepository;
 import org.sopt.poti.global.error.BusinessException;
 import org.sopt.poti.global.error.ErrorStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ArtistService {
 
-    private final ArtistRepository artistRepository;
+  private final ArtistRepository artistRepository;
+  private final MemberRepository memberRepository;
 
-    public Artist getById(Long artistId) {
-        return artistRepository.findById(artistId)
-                .orElseThrow(() -> new BusinessException(ErrorStatus.ARTIST_NOT_FOUND));
-    }
+  public Artist getById(Long artistId) {
+    return artistRepository.findById(artistId)
+        .orElseThrow(() -> new BusinessException(ErrorStatus.ARTIST_NOT_FOUND));
+  }
 
-    public ArtistListResponse getArtists() {
-        List<Artist> artists = artistRepository.findAll();
+  public ArtistListResponse getArtists() {
+    List<Artist> artists = artistRepository.findAll();
 
-        return new ArtistListResponse(
-                artists.stream()
-                        .map(a -> new ArtistListResponse.ArtistItemResponse(
-                                a.getId(),
-                                a.getName(),
-                                a.getLogoImageUrl()
-                        ))
-                        .toList()
-        );
-    }
+    return new ArtistListResponse(
+        artists.stream()
+            .map(a -> new ArtistListResponse.ArtistItemResponse(
+                a.getId(),
+                a.getName(),
+                a.getLogoImageUrl()
+            ))
+            .toList()
+    );
+  }
+
+  public MemberListResponse getMembers(Long artistId) {
+    Artist artist = artistRepository.findById(artistId)
+        .orElseThrow(() -> new BusinessException(ErrorStatus.ARTIST_NOT_FOUND));
+
+    List<MemberResponse> list = memberRepository.findByArtist(artist)
+        .stream()
+        .map(member -> new MemberResponse(member.getId(), member.getName()))
+        .toList();
+
+    return new MemberListResponse(list);
+  }
 }
