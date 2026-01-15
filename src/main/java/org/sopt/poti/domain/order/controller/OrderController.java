fd2c655698d7390abc59b5a15e3f51c2a273ee6b
88guri <quffl2002@naver.com>
2026-01-15 23:08:08 +0900

    chore: #39 오타수정

--- src/main/java/org/sopt/poti/domain/order/controller/OrderController.java
+++ src/main/java/org/sopt/poti/domain/order/controller/OrderController.java
@@ -14,25 +14,28 @@ import org.sopt.poti.global.security.UserPrincipal;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "Orders",description = "공구 주문 관련 API")
+@Tag(name = "Orders", description = "공구 주문 관련 API")
 @RequestMapping("/api/v1/orders")
 public class OrderController {
 
-    private final OrderCreateService orderCreateService;
+  private final OrderCreateService orderCreateService;
 
-    @Operation(summary = "공구 참여(주문) 폼 제축", description = "유저가 공구 참여(구매)를 위한 폼을 작성합니다.")
-    @PostMapping
-    public ResponseEntity<ApiResponse<CreateOrderResponse>> createOrder(
-            @AuthenticationPrincipal UserPrincipal userPrincipal,
-            @RequestBody @Valid CreateOrderRequest request
-    ) {
-        CreateOrderResponse data = orderCreateService.createOrder(userPrincipal.getUserId(), request);
+  @Operation(summary = "공구 참여(주문) 폼 제출", description = "유저가 공구 참여(구매)를 위한 폼을 작성합니다.")
+  @PostMapping
+  public ResponseEntity<ApiResponse<CreateOrderResponse>> createOrder(
+      @AuthenticationPrincipal UserPrincipal userPrincipal,
+      @RequestBody @Valid CreateOrderRequest request
+  ) {
+    CreateOrderResponse data = orderCreateService.createOrder(userPrincipal.getUserId(), request);
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ApiResponse.success(SuccessStatus.CREATED, data));
-    }
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(ApiResponse.success(SuccessStatus.CREATED, data));
+  }
 }
