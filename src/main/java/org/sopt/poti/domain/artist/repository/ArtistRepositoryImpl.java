a40248a28dc02b4d2a634a451be58cb6fb38b5b5
88guri <quffl2002@naver.com>
2026-01-18 22:22:18 +0900

    refactor: #87 아티스트 실시간 검색 응답값에 artistId 추가

--- src/main/java/org/sopt/poti/domain/artist/repository/ArtistRepositoryImpl.java
+++ src/main/java/org/sopt/poti/domain/artist/repository/ArtistRepositoryImpl.java
@@ -1,8 +1,10 @@
 package org.sopt.poti.domain.artist.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.sopt.poti.domain.artist.dto.response.ArtistTitlesResponse.ArtistTitleDto;
 import org.sopt.poti.domain.artist.entity.QArtist;
 
 @RequiredArgsConstructor
@@ -11,11 +13,17 @@ public class ArtistRepositoryImpl implements ArtistRepositoryCustom {
   private final JPAQueryFactory queryFactory;
 
   @Override
-  public List<String> findNamesByPrefix(String keyword, int limit) {
+  public List<ArtistTitleDto> findByPrefix(String keyword, int limit) {
     QArtist artist = QArtist.artist;
 
     return queryFactory
-        .selectDistinct(artist.name)
+        .selectDistinct(
+            Projections.constructor(
+                ArtistTitleDto.class,
+                artist.id,
+                artist.name
+            )
+        )
         .from(artist)
         .where(artist.name.startsWithIgnoreCase(keyword))
         .orderBy(artist.name.asc())
