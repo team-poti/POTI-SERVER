05803cba63b1dc9944246ddcb7c34cb6a27631a3
pbem22 <dlacofbs1229@gmail.com>
2026-01-20 23:03:05 +0900

    fix: 리뷰 반영

--- src/main/java/org/sopt/poti/domain/order/service/OrderService.java
+++ src/main/java/org/sopt/poti/domain/order/service/OrderService.java
@@ -99,16 +99,6 @@ public class OrderService {
       throw new BusinessException(ErrorStatus.ORDER_EXISTS_SHIPPINGS);
     }
 
-    long notShippedCount = orderRepository.countByGroupBuyPostIdAndStatusIn(
-        groupBuyPost.getId(),
-        List.of(OrderStatus.WAIT_PAY, OrderStatus.WAIT_PAY_CHECK, OrderStatus.PAID,
-            OrderStatus.READY)
-    );
-
-    if (notShippedCount == 0) { // 모두 배송 시작(SHIPPED) 이상
-      groupBuyPost.updateStatus(GroupBuyPostStatus.SHIPPING);
-    }
-
     LocalDateTime shippedAt = LocalDateTime.now();
 
     Delivery delivery = Delivery.builder()
@@ -121,6 +111,16 @@ public class OrderService {
     deliveryService.saveDelivery(delivery);
     order.startDelivery();
 
+    long notShippedCount = orderRepository.countByGroupBuyPostIdAndStatusIn(
+        groupBuyPost.getId(),
+        List.of(OrderStatus.WAIT_PAY, OrderStatus.WAIT_PAY_CHECK, OrderStatus.PAID,
+            OrderStatus.READY)
+    );
+
+    if (notShippedCount == 0) { // 모두 배송 시작(SHIPPED) 이상
+      groupBuyPost.updateStatus(GroupBuyPostStatus.SHIPPING);
+    }
+    
     return new StartDeliveryResponse(orderId, order.getStatus(),
         startDeliveryRequest.trackingNumber(), shippedAt);
   }
