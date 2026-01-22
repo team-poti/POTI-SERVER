fc1d4e84d7fbe4c3b9a45be0d6708a1cd8f84062
88guri <quffl2002@naver.com>
2026-01-22 23:59:32 +0900

    fix: #148 휴대폰 번호 검증 어노테이션 수정 반영

--- src/main/java/org/sopt/poti/domain/order/entity/DeliveryInfo.java
+++ src/main/java/org/sopt/poti/domain/order/entity/DeliveryInfo.java
@@ -2,6 +2,7 @@ package org.sopt.poti.domain.order.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -22,7 +23,8 @@ public class DeliveryInfo {
   private String addressLine;
 
   @Column(length = 20)
-  @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "휴대폰 번호 양식에 맞지 않습니다.")
+  @NotBlank(message = "휴대폰 번호는 필수입니다.")
+  @Pattern(regexp = "^01[016789]-\\d{3,4}-\\d{4}$", message = "휴대폰 번호 양식에 맞지 않습니다.")
   private String phone;
 
   public DeliveryInfo(
