bb4b4dc44167c387572b190dbe622296c96d30a7
pbem22 <dlacofbs1229@gmail.com>
2026-07-19 11:41:03 +0900

    fix: #210 테스트 환경에서 Firebase 초기화 제외

--- src/main/java/org/sopt/poti/global/config/FirebaseConfig.java
+++ src/main/java/org/sopt/poti/global/config/FirebaseConfig.java
@@ -7,10 +7,12 @@ import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
 @Configuration
+@Profile("!test")
 public class FirebaseConfig {
 
   @Value("${firebase.credentials-path}")
