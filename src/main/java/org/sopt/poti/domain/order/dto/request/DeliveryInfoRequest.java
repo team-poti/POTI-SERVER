5566fb0608fc21987ef8c4cfd3c7e7b5a26800e9
pbem22 <dlacofbs1229@gmail.com>
2026-08-15 21:35:01 +0900

    feat: #216 분철 참여 폼 필수값 validation 추가

--- src/main/java/org/sopt/poti/domain/order/dto/request/DeliveryInfoRequest.java
+++ src/main/java/org/sopt/poti/domain/order/dto/request/DeliveryInfoRequest.java
@@ -1,8 +1,10 @@
 package org.sopt.poti.domain.order.dto.request;
 
-public record DeliveryInfoRequest (
-        String receiverName,
-        String zipcode,
-        String addressLine,
-        String phone
-){ }
+import jakarta.validation.constraints.NotBlank;
+
+public record DeliveryInfoRequest(
+        @NotBlank(message = "수령인 이름을 입력해주세요.") String receiverName,
+        @NotBlank(message = "우편번호를 입력해주세요.") String zipcode,
+        @NotBlank(message = "주소를 입력해주세요.") String addressLine,
+        @NotBlank(message = "전화번호를 입력해주세요.") String phone
+) {}
