c6d45f0012968154a9e944d353e6878092e42954
임채륜 <59529734+PBEM22@users.noreply.github.com>
2026-08-17 11:51:28 +0900

    [FEAT] #216 분철 참여 폼 필수값 validation 추가 (#262)
    * feat: #216 분철 참여 폼 필수값 validation 추가
    
    * fix: #216 items 원소 null 검증 추가 (@NotNull @Valid)

--- src/main/java/org/sopt/poti/domain/order/dto/request/CreateOrderRequest.java
+++ src/main/java/org/sopt/poti/domain/order/dto/request/CreateOrderRequest.java
@@ -1,11 +1,14 @@
 package org.sopt.poti.domain.order.dto.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
-public record CreateOrderRequest (
-        Long groupBuyPostId,
-        Long shippingId,
-        DeliveryInfoRequest deliveryInfo,
-        List<OrderItemRequest> items,
+public record CreateOrderRequest(
+        @NotNull(message = "분철글 ID를 입력해주세요.") Long groupBuyPostId,
+        @NotNull(message = "배송 방법을 선택해주세요.") Long shippingId,
+        @NotNull(message = "배송지 정보를 입력해주세요.") @Valid DeliveryInfoRequest deliveryInfo,
+        @NotEmpty(message = "주문 항목을 선택해주세요.") List<@NotNull(message = "주문 항목을 입력해주세요.") @Valid OrderItemRequest> items,
         boolean saveAsMyAddress
-){}
+) {}
