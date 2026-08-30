7cdefebf244e3e72857687d0de0e643af76a9108
pbem22 <dlacofbs1229@gmail.com>
2026-08-31 00:22:27 +0900

    fix: #287 deleteByToken @Transactional 추가로 afterCommit 경로 삭제 보장

--- src/main/java/org/sopt/poti/domain/fcmtoken/repository/FcmTokenRepository.java
+++ src/main/java/org/sopt/poti/domain/fcmtoken/repository/FcmTokenRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.sopt.poti.domain.fcmtoken.entity.FcmToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
@@ -13,7 +14,9 @@ public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
 
   List<FcmToken> findAllByUser_Id(Long userId);
 
+  @Transactional
   void deleteAllByUser_Id(Long userId);
 
+  @Transactional
   void deleteByToken(String token);
 }
